3b02839063ce2f8a18d7145c2788661c9d593eb2
Shannon Lounges <shannonlounges@Shannons-MacBook-Pro.local>
2020-10-17 19:19:00 -0400

    adding data with ajax

--- js/main.js
+++ js/main.js
@@ -2,14 +2,24 @@
 
 //make an AJAX request using the FETCH API
 fetch('./data/classData.json') //fetch statement is an ajax request
-.then(res => res.json())//take whatver json object into js
+.then(res => res.json())//take whatever json object into js
 .then(data => {
+
+        //debugger;
     console.log(data); //gives you back the data so that you can do whatever you want
 
-    //handleData
+    handleData(data);
     //here's where you would call the function that puts the pieces on the page
 })
 .catch((err) => { //if it breaks tell me what happened
     console.log(err);
 })
+
+function handleData (data){
+    let coursename = document.querySelector(".profPanelText .text-muted");
+   
+    
+    coursename.innerHTML = data.coursename + " - " + data.coursecode;
+
+}
 })();
